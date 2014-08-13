931c2ada047c68d0e9fe741f6c4532ffd10d0587
Naveen <naveen246@gmail.com>
2014-08-13 16:16:27 +0530

    2 min fast log taking 3 min with java client

--- Client.java
+++ Client.java
@@ -2,20 +2,38 @@
 
 import java.net.*;
 import java.io.*;
+import java.nio.channels.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
+        URL url = new URL("http://169.254.101.102/?log=f#");
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream("log.txt");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 
-        URL url = new URL("http://169.254.101.102");
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-        PrintWriter fout = new PrintWriter("log.txt");
+        FileInputStream in = null;
+        PrintWriter out = null;
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            //System.out.println(inputLine);
-            fout.println(inputLine);
+        try {
+            in = new FileInputStream("log.txt");
+            out = new PrintWriter("out.txt");
+            int val, count = 0;
+            int c, d;
+
+            while ( ((c = in.read()) != -1) && ((d = in.read()) != -1) )  {
+                val = ((c << 8) | d);
+                out.print(Integer.toString(val) + ',');
+                count++;
+                if(count % 4 == 0)
+                    out.println();
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
         }
-        in.close();
-        fout.close();
     }
 }
